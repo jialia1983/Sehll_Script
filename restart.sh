f4f9214dc1777ef04d5a0e8d61295bd5566f1db1
jialia1983 <jialia@qq.com>
2017-08-25 09:55:48 +0800

    增加功能
    在for循环里增加了对端口是否存在的判断

--- restart.sh
+++ restart.sh
@@ -1,30 +1,44 @@
-#!/bin/bash
-#gent和service。反之则不作操作。
+#!/bin/bash -vx
+#Name:restart.sh
+#Version:1.0
+#Date:2017-8-24
+#Author:jiangliang
 function get_pid_int
 {
   echo $(echo `netstat -lntup | awk '/^tcp/{print $7}' | grep "$pid_name$"` | cut -d "/" -f 1)
 }
 
+#exec > /home/restart.log
+sleep_number="10s"
 pid_name="httpd"
 Port="80"
 restart_command="systemctl restart $pid_name"
 http_code=`netstat -lntup | awk '/^tcp/{print $4}' | grep -q ":$Port$";echo $?`
 pid_old=$(get_pid_int)
-
-
-# 当httpd已经启动时，做重启操作
+#echo $pid_old
+# 当服务已经启动时，做重启操作
 if [ $http_code -eq 0 ]
 then
-  echo "服务端口存在,重启$Service服务......"
-  $restart_command  
-  pid_new=$(get_pid_int)
-  #echo $pid_new 
-    if [ "x$pid_old" != "x$pid_new" ];then echo "重启服务$pid_name成功！";exit 0;
-    else
-    echo "重启服务$Serviice失败！"
-	exit
+  	echo "$pid_name 重新启动中......"
+  	$restart_command
+  	if [ $http_code != 0 ]
+	then
+		for (( i=1; i<= 3; i++))
+		do
+		 sleep $sleep_number
+		 if [ $http_code -eq 0 ]
+		 then
+			exit 0;
+		fi
+		done
 	fi
+		 pid_new=$(get_pid_int)	
+  	#echo $pid_new 
+    	if [ $http_code -eq 0 ]  && [ "x$pid_old" != "x$pid_new" ];then echo "重启 $pid_name 成功！";exit 0;
+    	else
+    	echo "重启 $pid_name 失败！"
+		exit
+		fi
 fi
-
-# 默认端口不存在，不执行
-echo "服务端口不存在,不进行操作！"
+# 当服务没有启动则不进行操作！
+echo "服务不存在！"
